b6b6bec7f4fb436d79d31379ae5a14f74b6b2cb7
Sandeep Gill <sandeepgill62@gmail.com>
2023-06-05 05:12:34 +0000

    add url_new and form

--- express_server.js
+++ express_server.js
@@ -2,22 +2,35 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+//set up for ejs template
 app.set("view engine", "ejs");
+
+//to translate and parse the data
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// GET route to show form
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// Route Parameters
 app.get("/urls/:id", (req, res) => {
-  //const templateVars = { id: req.params.id };
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  console.log(templateVars.id);
   res.render("urls_show", templateVars);
 });
 
